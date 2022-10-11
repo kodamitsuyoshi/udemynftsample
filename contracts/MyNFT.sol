361c8739797dbd9a7dd8b7bc8b55c6c9b0873611
Remix Workspace <>
2022-10-11 19:55:01 +0900

    add contracts

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MyNFT is ERC721 {
 
 
-    constructor() ERC721("MyNFT","MYNFT"){
+    constructor() ERC721("MyNFTkoda","MYNFTK"){
 
     }
 
--- contracts/OffURIChangeable.sol
+++ contracts/OffURIChangeable.sol
@@ -3,12 +3,12 @@
 pragma solidity ^0.8.14;
 import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
-
-contract OZOnlyOwnerMintWithModifier is ERC721 , Ownable {
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
+contract OffURIChangeable is ERC721URIStorage , Ownable {
 
     
 
-    constructor() ERC721("OnlyOwnerMint","OZKOWNERMOD"){
+    constructor() ERC721("TecoOffURIChangeable","TOUC"){
        
     }
 
@@ -20,6 +20,10 @@ contract OZOnlyOwnerMintWithModifier is ERC721 , Ownable {
     */
     function nftMint(uint256 tokenId) public onlyOwner{
         _mint(_msgSender(),tokenId);
+    }
+
+    function setTokenURI( uint256 tokenId,  string calldata uri) public onlyOwner{
+        _setTokenURI(tokenId,uri);
     }
 }
 
